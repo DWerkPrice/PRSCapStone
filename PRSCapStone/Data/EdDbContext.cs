6eebe67f4d2d8492c2e6e02ca8a30185fa6c5c92
David Price <dwerkprice@gmail.com>
2020-03-09 17:29:16 -0400

    Add project files.

---
+++ PRSCapStone/Data/EdDbContext.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PRSCapStone.Models;
+
+namespace PRSCapStone.Data
+{
+    public class EdDbContext : DbContext
+    {
+        public EdDbContext (DbContextOptions<EdDbContext> options)
+            : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder model) {
+            model.Entity<User>(e => {
+                e.ToTable("Users");
+                e.HasKey(x => x.Id);
+                e.Property(x => x.Username).HasMaxLength(30).IsRequired();
+                e.Property(x => x.Password).HasMaxLength(30).IsRequired();
+                e.Property(x => x.Firstname).HasMaxLength(30).IsRequired();
+                e.Property(x => x.Lastname).HasMaxLength(30).IsRequired();
+                e.Property(x => x.Phone).HasMaxLength(12);
+                e.Property(x => x.Email).HasMaxLength(255);
+                e.Property(x => x.IsReviewer).IsRequired().HasDefaultValue(false);
+                e.Property(x => x.IsAdmin).IsRequired().HasDefaultValue(false);
+                e.HasIndex(x => x.Username).IsUnique();
+
+            });
+
+            model.Entity<Vendor>(e => {
+                e.ToTable("Vendors");
+                e.HasKey(x => x.Id);
+                e.Property(x => x.Code).HasMaxLength(30).IsRequired();
+                e.Property(x => x.Name).HasMaxLength(30).IsRequired();
+                e.Property(x => x.Address).HasMaxLength(30).IsRequired();
+                e.Property(x => x.City).HasMaxLength(30).IsRequired();
+                e.Property(x => x.State).HasMaxLength(2).IsRequired();
+                e.Property(x => x.Zip).HasMaxLength(5).IsRequired();
+                e.Property(x => x.Phone).HasMaxLength(12);
+                e.Property(x => x.Email).HasMaxLength(255);
+                e.HasIndex(x => x.Code).IsUnique();
+            });
+
+            model.Entity<Product>(e => {
+                e.ToTable("Products");
+                e.HasKey(x => x.Id);
+                e.Property(x => x.PartNbr).HasMaxLength(30).IsRequired();
+                e.Property(x => x.Name).HasMaxLength(30).IsRequired();
+                e.Property(x => x.Unit).HasMaxLength(30).IsRequired();
+                e.Property(x => x.Price).HasColumnType("decimal(11,2)").IsRequired();
+                e.Property(x => x.PhotoPath).HasMaxLength(255);
+                e.Property(x => x.PartNbr).IsRequired();
+                e.HasIndex(x => x.PartNbr).IsUnique();
+            });
+
+            model.Entity<Request>(e => {
+                e.ToTable("Requests");
+                e.HasKey(x => x.Id);
+                e.Property(x => x.Description).HasMaxLength(80).IsRequired();
+                e.Property(x => x.Justification).HasMaxLength(80).IsRequired();
+                e.Property(x => x.RejectionReason).HasMaxLength(80);
+                e.Property(x => x.DeliveryMode).HasMaxLength(20).HasDefaultValue("Pickup").IsRequired();
+                e.Property(x => x.Status).HasMaxLength(10).HasDefaultValue("New").IsRequired();
+                e.Property(x => x.Total).HasColumnType("decimal(11,2)").HasDefaultValue("0").IsRequired();
+            });
+
+        }
+        public DbSet<PRSCapStone.Models.User> User { get; set; }
+        public DbSet<PRSCapStone.Models.Vendor> Vendor { get; set; }
+        public DbSet<PRSCapStone.Models.Product> Product { get; set; }
+        public DbSet<PRSCapStone.Models.Request> Request { get; set; }
+
+    }
+}
---
+++ PRSCapStone/Migrations/20200309211816_fixrequestdefaults.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PRSCapStone.Migrations
+{
+    public partial class fixrequestdefaults : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Requests",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "New",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10,
+                oldDefaultValue: "'New'");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeliveryMode",
+                table: "Requests",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "Pickup",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldDefaultValue: "'Pickup'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Requests",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "'New'",
+                oldClrType: typeof(string),
+                oldMaxLength: 10,
+                oldDefaultValue: "New");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeliveryMode",
+                table: "Requests",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "'Pickup'",
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldDefaultValue: "Pickup");
+        }
+    }
+}
